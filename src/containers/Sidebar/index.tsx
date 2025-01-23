b392e5c218bb1a6dddf8b15d239c38a12370cf7f
Caio <caiocesarpoke2@hotmail.com>
2025-01-23 15:45:24 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/CaioPO2.png" />
+    <Titulo fontSize={20}>Caio Pontes</Titulo>
+  </aside>
+)
+
+export default Sidebar
