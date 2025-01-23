3894155be7b56dc2e75e44e3145663db748a0112
Caio <caiocesarpoke2@hotmail.com>
2025-01-23 17:06:41 -0300

    terminado o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import TemaLight from './themes/light'
+import TemaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function TrocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? TemaDark : TemaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={TrocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
