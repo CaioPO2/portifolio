aca5d76cb93ac54135bf73b3144904ab20d624d3
Caio <caiocesarpoke2@hotmail.com>
2025-01-23 16:33:08 -0300

    estilizando a SideBar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/CaioPO2.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Title'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/CaioPO2.png" />
-    <Titulo fontSize={20}>Caio Pontes</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Caio Pontes</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        CaioPO2
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Full-Stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
